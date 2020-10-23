97e4dae5ac488d18d01d4b4bd92ccf4b6e6504a0
paulcarlton-ww <paul.carlton@fmr.com>
2020-10-23 19:54:18 +0100

    add to debugging

--- controllers/addons_controller.go
+++ controllers/addons_controller.go
@@ -386,7 +386,8 @@ func (r *AddonsLayerReconciler) repoMapperFunc(a handler.MapObject) []reconcile.
 		r.Log.Error(fmt.Errorf("unable to cast object to GitRepository"), "skipping processing", "kind", "gitrepositories.source.toolkit.fluxcd.io", "data", apply.LogJSON(srcRepo))
 		return []reconcile.Request{}
 	}
-	r.Log.V(1).Info("monitoring", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Name, "data", apply.LogJSON(srcRepo))
+	r.Log.V(1).Info("monitoring", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace",
+		srcRepo.Namespace, "name", srcRepo.Name, "data", apply.LogJSON(srcRepo))
 	addonsList := &kraanv1alpha1.AddonsLayerList{}
 	if err := r.List(r.Context, addonsList); err != nil {
 		r.Log.Error(err, "unable to list AddonsLayers", "kind", "gitrepositories.source.toolkit.fluxcd.io",
@@ -398,7 +399,8 @@ func (r *AddonsLayerReconciler) repoMapperFunc(a handler.MapObject) []reconcile.
 	for _, addon := range addonsList.Items {
 		layer := layers.CreateLayer(r.Context, r.Client, r.k8client, r.Log, &addon) //nolint:scopelint // ok
 		if layer.GetSpec().Source.Name == srcRepo.Name && layer.GetSpec().Source.NameSpace == srcRepo.Namespace {
-			r.Log.V(1).Info("layer is using this source", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Name, "layer", addon.Name)
+			r.Log.V(1).Info("layer is using this source", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace",
+				srcRepo.Namespace, "name", srcRepo.Name, "revision", srcRepo.Status.Artifact.Revision, "layer", addon.Name)
 			layerList = append(layerList, layer)
 			addons = append(addons, reconcile.Request{NamespacedName: types.NamespacedName{Name: layer.GetName(), Namespace: ""}})
 		}
@@ -408,20 +410,25 @@ func (r *AddonsLayerReconciler) repoMapperFunc(a handler.MapObject) []reconcile.
 	}
 	repo := r.Repos.Get(r.Repos.PathKey(srcRepo))
 	if repo == nil {
-		r.Log.V(1).Info("new repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Namespace)
+		r.Log.V(1).Info("new repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io",
+			"namespace", srcRepo.Namespace, "name", srcRepo.Name, "revision", srcRepo.Status.Artifact.Revision)
 	} else {
 		if srcRepo.Status.Artifact.Revision == repo.GetGitRepo().Status.Artifact.Revision {
-			r.Log.V(1).Info("unchanged repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Namespace)
+			r.Log.V(1).Info("unchanged repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io",
+				"namespace", srcRepo.Namespace, "name", srcRepo.Name, "revision", srcRepo.Status.Artifact.Revision)
 		}
 		return []reconcile.Request{}
 	}
 	repo = r.Repos.Add(srcRepo)
-	r.Log.V(1).Info("created repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Namespace)
+	r.Log.V(1).Info("created repo object", "kind", "gitrepositories.source.toolkit.fluxcd.io",
+		"namespace", srcRepo.Namespace, "name", srcRepo.Namespace, "revision", srcRepo.Status.Artifact.Revision)
 	if err := repo.SyncRepo(); err != nil {
-		r.Log.Error(err, "unable to sync repo, not requeuing", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Name)
+		r.Log.Error(err, "unable to sync repo, not requeuing", "kind", "gitrepositories.source.toolkit.fluxcd.io",
+			"namespace", srcRepo.Namespace, "name", srcRepo.Name, "revision", srcRepo.Status.Artifact.Revision)
 		return []reconcile.Request{}
 	}
-	r.Log.V(1).Info("synced repo", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Name)
+	r.Log.V(1).Info("synced repo", "kind", "gitrepositories.source.toolkit.fluxcd.io",
+		"namespace", srcRepo.Namespace, "name", srcRepo.Name, "revision", srcRepo.Status.Artifact.Revision)
 
 	for _, layer := range layerList {
 		if err := repo.LinkData(layer.GetSourcePath(), layer.GetSpec().Source.Path); err != nil {
@@ -431,7 +438,8 @@ func (r *AddonsLayerReconciler) repoMapperFunc(a handler.MapObject) []reconcile.
 			continue
 		}
 	}
-	r.Log.Info("synced source", "kind", "gitrepositories.source.toolkit.fluxcd.io", "namespace", srcRepo.Namespace, "name", srcRepo.Name, "layers", addons)
+	r.Log.Info("synced source", "kind", "gitrepositories.source.toolkit.fluxcd.io", 
+		"namespace", srcRepo.Namespace, "name", srcRepo.Name,"revision", srcRepo.Status.Artifact.Revision, "layers", addons)
 	return addons
 }
 

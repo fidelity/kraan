8f8876c5cdcef574b60c6cc12fd88f6c3df10092
Paul Carlton <paul.carlton@weave.works>
2020-11-02 08:51:03 +0000

    create testdata branch

--- controllers/addons_controller.go
+++ controllers/addons_controller.go
@@ -39,6 +39,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
 	kraanv1alpha1 "github.com/fidelity/kraan/api/v1alpha1"
 	"github.com/fidelity/kraan/pkg/apply"
 	"github.com/fidelity/kraan/pkg/layers"

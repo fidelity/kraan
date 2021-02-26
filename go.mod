701df18d78d890a488a12285665eba9cc7985570
paulcarlton-ww <paul.carlton@fmr.com>
2021-02-26 13:14:10 -0500

    unit tests - wip

--- go.mod
+++ go.mod
@@ -3,39 +3,23 @@ module github.com/fidelity/kraan
 go 1.14
 
 require (
-	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/fluxcd/helm-controller/api v0.3.0
 	github.com/fluxcd/pkg/apis/meta v0.4.0
-	github.com/fluxcd/pkg/runtime v0.3.0
 	github.com/fluxcd/pkg/untar v0.0.5
 	github.com/fluxcd/source-controller/api v0.3.0
-	github.com/go-delve/delve v1.5.0 // indirect
 	github.com/go-logr/logr v0.4.0
 	github.com/go-logr/zapr v0.4.0 // indirect
 	github.com/golang/mock v1.4.4
 	github.com/google/go-cmp v0.5.2
-	github.com/google/go-dap v0.3.0 // indirect
-	github.com/jessevdk/go-flags v1.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/onsi/ginkgo v1.12.1
-	github.com/onsi/gomega v1.10.1
-	github.com/paulcarlton-ww/go-utils v0.0.0-20200729094929-4657992b390c // indirect
-	github.com/peterh/liner v1.2.0 // indirect
+	github.com/paulcarlton-ww/goutils/pkg/testutils v0.0.41
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.7.1
-	github.com/sirupsen/logrus v1.7.0 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
-	go.starlark.net v0.0.0-20200929122913-88a10930eb75 // indirect
 	go.uber.org/zap v1.16.0
-	golang.org/x/arch v0.0.0-20200826200359-b19915210f00 // indirect
 	golang.org/x/mod v0.3.0
-	golang.org/x/sys v0.0.0-20201005065044-765f4ea38db3 // indirect
 	golang.org/x/tools v0.0.0-20201002184944-ecd9fd270d5d // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
 	k8s.io/api v0.19.4
 	k8s.io/apiextensions-apiserver v0.19.4
-	k8s.io/apimachinery v0.19.4
+	k8s.io/apimachinery v0.20.2
 	k8s.io/client-go v0.19.4
 	sigs.k8s.io/controller-runtime v0.6.3
 )
--- pkg/apply/applier_test.go
+++ pkg/apply/applier_test.go
@@ -1,7 +1,9 @@
 package apply_test
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"testing"
 	"time"
@@ -10,10 +12,13 @@ import (
 	"github.com/go-logr/logr"
 	testlogr "github.com/go-logr/logr/testing"
 	gomock "github.com/golang/mock/gomock"
+	"github.com/paulcarlton-ww/goutils/pkg/testutils"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	types "k8s.io/apimachinery/pkg/types"
+	fakeK8s "k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
@@ -21,15 +26,19 @@ import (
 	"github.com/fidelity/kraan/pkg/apply"
 	"github.com/fidelity/kraan/pkg/internal/kubectl"
 	kubectlmocks "github.com/fidelity/kraan/pkg/internal/mocks/kubectl"
+	"github.com/fidelity/kraan/pkg/layers"
 )
 
 const (
-	addonsFileName       = "addons.json"
-	helmrReleasesFileName = "helmreleases.json"
+	addonsFileName       = "testdata/addons.json"
+	helmReleasesFileName = "testdata/helmreleases.json"
+	bootstrapOrphaned    = "bootstrap/orphaned"
+	appLayer             = "apps"
 )
 
 var (
 	testScheme = runtime.NewScheme()
+	fakeClient client.Client
 )
 
 func init() {
@@ -57,6 +66,29 @@ func getAddonsFromFile(t *testing.T, fileName string) *kraanv1alpha1.AddonsLayer
 	return addons
 }
 
+func getAddonFromList(t *testing.T, name string, addonList *kraanv1alpha1.AddonsLayerList) *kraanv1alpha1.AddonsLayer {
+	for _, item := range addonList.Items {
+		if item.ObjectMeta.Name == name {
+			return &item
+		}
+	}
+
+	t.Fatalf("failed to find AddonsLayer: %s in list", name)
+
+	return nil
+}
+
+func getLayer(t *testing.T, layerName, dataFileName string) layers.Layer {
+	fakeK8sClient := fakeK8s.NewSimpleClientset()
+	data := getAddonFromList(t, layerName, getAddonsFromFile(t, dataFileName))
+	if data == nil {
+		t.Fatalf("failed to read AddonsLayerList file: %s", dataFileName)
+		return nil
+	}
+	fakeRecorder := record.NewFakeRecorder(1000)
+	return layers.CreateLayer(context.Background(), fakeClient, fakeK8sClient, logr.Discard(), fakeRecorder, testScheme, data)
+}
+
 func getHelmReleasesFromFile(t *testing.T, fileName string) *helmctlv2.HelmReleaseList {
 	buffer, err := ioutil.ReadFile(fileName)
 	if err != nil {
@@ -76,8 +108,19 @@ func getHelmReleasesFromFile(t *testing.T, fileName string) *helmctlv2.HelmRelea
 	return helmReleases
 }
 
-func getApplierParams(t *testing.T, execpassName,
-	addonLayersFileName, helmReleasesFileName string,
+func getHelmReleaseFromList(t *testing.T, nameSpaceSlashName string, helmReleaseList *helmctlv2.HelmReleaseList) *helmctlv2.HelmRelease {
+	for _, item := range helmReleaseList.Items {
+		if fmt.Sprintf("%s/%s", item.Namespace, item.Name) == nameSpaceSlashName {
+			return &item
+		}
+	}
+
+	t.Fatalf("failed to find HelmRelease: %s in list", nameSpaceSlashName)
+
+	return nil
+}
+
+func getApplierParams(t *testing.T, addonsFileName, helmReleasesFileName string,
 	client client.Client, scheme *runtime.Scheme) []interface{} {
 	addonsList := getAddonsFromFile(t, addonsFileName)
 
@@ -112,6 +155,16 @@ func createApplier(t *testing.T, params []interface{}) apply.LayerApplier {
 		t.Fatalf("failed to create applier, %s", err)
 	}
 	return applier
+}
+
+func castToApplier(t *testing.T, i interface{}) apply.LayerApplier {
+	if ep, ok := i.(apply.LayerApplier); ok {
+		return ep
+	}
+
+	t.Fatalf("failed to cast interface to apply.LayerApplier")
+
+	return nil
 }
 
 func fakeAddonsLayer(sourcePath, layerName string, layerUID types.UID) *kraanv1alpha1.AddonsLayer { //nolint
@@ -183,4 +236,52 @@ func TestNewApplierWithMockKubectl(t *testing.T) {
 		t.Fatalf("The NewApplier constructor returned an error: %s", err)
 	}
 	t.Logf("NewApplier returned (%T) %#v", applier, applier)
+}
+
+func TestGetOrphanedHelmReleases(t *testing.T) {
+	tests := []*testutils.DefTest{
+		{
+			Number:      1,
+			Description: "one orphaned helm release, not this layer",
+			Config: createApplier(t, getApplierParams(t,
+				addonsFileName,
+				helmReleasesFileName,
+				nil, testScheme)),
+			Inputs: []interface{}{
+				context.Background(),
+				getLayer(t, appLayer, addonsFileName),
+			},
+			Expected: []interface{}{
+				map[string]*helmctlv2.HelmRelease{
+					bootstrapOrphaned: getHelmReleaseFromList(t, bootstrapOrphaned,
+						getHelmReleasesFromFile(t, helmReleasesFileName)),
+				},
+				nil,
+			},
+			ResultsCompareFunc: testutils.CompareJSON,
+		},
+	}
+
+	testFunc := func(t *testing.T, testData *testutils.DefTest) bool {
+		u := testutils.NewTestUtil(t, testData)
+
+		u.CallPrepFunc()
+
+		a := castToApplier(t, testData.Config)
+
+		hrs, err := a.GetOrphanedHelmReleases(
+			testData.Inputs[0].(context.Context),
+			testData.Inputs[1].(layers.Layer))
+		testData.Results = []interface{}{hrs, err}
+
+		return u.CallCheckFunc()
+	}
+
+	for _, test := range tests {
+		if !testFunc(t, test) {
+			t.Fatalf("Test failed")
+
+			return
+		}
+	}
 }

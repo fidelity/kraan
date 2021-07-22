35c756265765c0e3fdb0ed0b8c4b3f40591fb6a7
Wang, Li <li.wang3@fmr.com>
2021-07-22 20:15:39 +0800

    add timeout when create kind cluster

--- controllers/suite_integration_test.go
+++ controllers/suite_integration_test.go
@@ -101,6 +101,8 @@ func startKindCluster(logf logr.Logger) {
 		err = p.Create(kindClusterName)
 		Expect(err).NotTo(HaveOccurred())
 		logf.Info("kind cluster created", "cluster", kindClusterName)
+		logf.Info("sleep 60 seconds to wait the node get ready")
+		time.sleep(60 * time.Second)
 	} else {
 		logf.Info("using existing kind cluster", "cluster", kindClusterName)
 		deleteKind = false

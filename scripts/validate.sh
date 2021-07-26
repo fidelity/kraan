336d37740afac4a682b650c03e3348c9b9bc65e5
Wang, Li <li.wang3@fmr.com>
2021-07-26 10:50:16 +0800

    update version validate check

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -5,29 +5,29 @@ echo "Comparing against $BASE_REF"
 git diff --name-status "$BASE_REF" | tee
 if [[ ${CHART_APP_VERSION} != "${VERSION}" ]]; then
   echo "❌ chart/Chart.yaml appVersion '${CHART_APP_VERSION}' must match ./VERSION '${VERSION}'"
-  exit 1
+  return false
 fi
 
 if [[ ${CHART_APP_VERSION} != "${CHART_VERSION}" ]]; then
   echo "❌ chart/Chart.yaml appVersion '${CHART_APP_VERSION}' must match chart/Chart.yaml Version '${CHART_VERSION}'"
-  exit 1
+  return false
 fi
 
 if ! git diff "$BASE_REF" -- chart/Chart.yaml | grep '+version:'; then
   if git diff --name-status "$BASE_REF" | grep -E 'chart/values.yaml|chart/templates'; then
     echo "❌ Chart.yaml version must be changed whenever chart changes occur"
-    exit 1
+    return false
   fi
   if git diff "$BASE_REF" -- chart/Chart.yaml | grep '+appVersion:'; then
     echo "❌ Chart.yaml appVersion was changed but version was not"
-    exit 1
+    return false
   fi
 fi
 
 if ! git diff "$BASE_REF" -- VERSION | grep '+'; then
   if git diff --name-status "$BASE_REF" | grep -v "_test.go" | grep -E '\.go$|go\.mod|go\.sum|Dockerfile'; then
     echo "❌ VERSION was not changed even though relevant code changes occured"
-    exit 1
+    return false
   fi
 fi
 
